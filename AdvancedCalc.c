7df182c9d9b8afcca6a79b6b0f88d90570979167
Cris M <crismecea1@outlook.com>
2026-01-04 23:39:28 -0500

    Added Basic and Conversion Menus

--- AdvancedCalc.c
+++ AdvancedCalc.c
@@ -2,12 +2,26 @@
 
 void main_menu();
 void basic_math();
-void history();
-void advanced_math();
+void settings();
+void scientific_math();
+void conversion();
+int user_choice();
 
 int main() {
-    main_menu();
-    basic_math();
+
+    int choice;
+    while (1) {
+        main_menu();
+        choice = user_choice();
+        
+        if (!choice) {
+            printf("Exiting Program...\n");
+            break;
+        }
+
+        
+    }
+    
     return 0;
 }
 
@@ -29,4 +43,43 @@ void basic_math() {
     printf("4. Division\n");
     printf("0. Quit\n");
     printf("Choose an option: ");
+}
+
+void settings() {
+    printf("\n===== Settings =====");
+    printf("1. See history\n");
+    printf("2. Clear History\n");
+    printf("3. Save to a File\n");
+    printf("0. Quit\n");
+    printf("Choose an option: ");
+}
+
+void conversions() {
+    printf("\n==== Conversions ====\n\n");
+    printf("1. Temperature (F to C)\n");
+    printf("2. Temperature (C to F)\n");
+    printf("3. Feet to Inches\n");
+    printf("4. Inches to Feet\n");
+    printf("5. Pounds to Kilos\n");
+    printf("0. Quit\n");
+    printf("Choose an option: ");
+}
+
+void scientific_math() {
+    printf("\n==== Scientific/Utility ====\n\n");
+    printf("1. Quadratic Equation\n");
+    printf("2. Square Root\n");
+    printf("3. LCM\n");
+    printf("4. Power\n");
+    printf("5. Factorial\n");
+    printf("0. Quit\n");
+    printf("Choose an option: ");
+
+}
+
+int user_choice(void) {
+    int x;
+    scanf("%i", &x);
+    while (getchar() != '\n');
+    return x;
 }
