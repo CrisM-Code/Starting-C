7ea6786b675e6e0f4b8be882646f906f57b8a83a
Cris M <crismecea1@outlook.com>
2026-01-07 22:21:43 -0500

    Added more functions: save history and view

--- AdvancedCalc.c
+++ AdvancedCalc.c
@@ -1,4 +1,34 @@
 #include <stdio.h>
+#define MAX_HISTORY 100
+#define MAX_LEN 100
+
+typedef struct {
+    char problem[MAX_LEN];
+} Main;
+
+Main main_history[MAX_HISTORY];
+int count = 0;
+
+typedef struct {
+    int a;
+    int b;
+    char op;
+    int result;
+} History;
+
+typedef struct {
+    int a;
+    char op1;
+    int b;
+    char op2;
+} History2;
+
+History2 history2[MAX_HISTORY];
+
+History history[MAX_HISTORY];
+int history_count = 0;
+
+FILE *fp;
 
 void main_menu();
 void basic_math();
@@ -8,6 +38,10 @@ void conversions();
 int user_choice();
 int first_num();
 int second_num();
+//void save_file();
+void save_history(int a, int b, char op, int result);
+void save_history2(int a, char op1, int b, char op2);
+void view_history();
 
 int main() {
 
@@ -67,7 +101,7 @@ void basic_math() {
         choice2 = user_choice();
     
         if (!choice2) {
-            printf("Exiting to Main Menu...");
+            printf("\nExiting to Main Menu...\n");
             break;
         }
         
@@ -75,25 +109,37 @@ void basic_math() {
             case 1: {
                 int x = first_num();
                 int y = second_num();
-                printf("%i + %i = %i\n", x, y, (x + y));
+                int result = x + y;
+                printf("%i + %i = %i\n", x, y, result);
+                save_history(x , y, "+", result);
                 break;
             }
             case 2: {
                 int x = first_num();
                 int y = second_num();
-                printf("%i - %i = %i\n", x, y, (x - y));
+                int result = x - y;
+                printf("%i - %i = %i\n", x, y, result);
+                save_history(x, y, "-", result);
                 break;
             }
             case 3:  {
                 int x = first_num();
                 int y = second_num();
-                printf("%i x %i = %i\n", x, y, (x*y));
+                int result = x * y;
+                printf("%i x %i = %i\n", x, y, result);
+                save_history(x, y, "*", result);
                 break;
             }
             case 4: {
                 int x = first_num();
                 int y = second_num();
+                if (y == 0) {
+                    printf("Error: Division by zero!\n");
+                    break;
+                }
+                int result = x / y;
                 printf("%i / %i = %i\n", x, y, (x/y));
+                save_history(x, y, "/", result);
                 break;
             }
             default: {
@@ -127,7 +173,7 @@ void conversions() {
         choice3 = user_choice();
 
         if (!choice3) {
-            printf("Exiting to Main Menu...");
+            printf("\nExiting to Main Menu...");
             break;
         }
 
@@ -138,6 +184,7 @@ void conversions() {
                 scanf("%f", &x);
                 float temp = 5/9 * (x-32);
                 printf("%f F = %f C\n", x, temp);
+                save_history2(x, "F", temp, "C");
                 break;
             }
             case 2: {
@@ -146,6 +193,7 @@ void conversions() {
                 scanf("%f", &x);
                 float temp = 9/5 * x + 32;
                 printf("%f C = %f F\n", x, temp);
+                save_history2(x, "C", temp, "F");
                 break;
             }
             case 3: {
@@ -154,6 +202,7 @@ void conversions() {
                 scanf("%f", &x);
                 float length = x * 12;
                 printf("%f ft = %f in\n", x, length);
+                save_history2(x, "ft", length, "in");
                 break;
             }
             case 4: {
@@ -162,6 +211,7 @@ void conversions() {
                 scanf("%f", &x);
                 float length = x / 12;
                 printf("%f in = %f ft\n", x, length);
+                save_history2(x, "in", length, "ft");
                 break;
             }
             case 5: {
@@ -169,7 +219,8 @@ void conversions() {
                 float x;
                 scanf("%f", &x);
                 float weight = x / 2.2;
-                printf("%f ft = %f in\n", x, weight);
+                printf("%f lb = %f kg\n", x, weight);
+                save_history2(x, "lb", weight, "kg");
                 break;
             }
             default:
@@ -211,4 +262,55 @@ int second_num(void) {
     printf("Enter a number: ");
     scanf("%i", &b);
     return b;
+}
+
+/*
+void save_file() {
+    fp = fopen("CalcHistory.txt", "w");
+    if (fp == NULL) {
+        printf("File Error!\n");
+        return;
+    }
+
+    for (int i = 0; i < history_count; i++) {
+        fprintf("CalcHistory.txt", "%i|%s\n", (i + 1), history[i].history);
+    }
+
+    fclose(fp);
+} */
+
+void save_history(int a, int b, char op, int result) {
+    if (history_count < MAX_HISTORY) {
+        history[history_count].a = a;
+        history[history_count].b = b;
+        history[history_count].op = op;
+        history[history_count].result = result;
+        main_history[history_count].problem == "%i %c %i = %i\n", a, b, op, result;
+        history_count++;
+    } else {
+        printf("History is full!\n");
+    }
+    
+}
+
+void save_history2(int a, char op1, int b, char op2) {
+    if(history_count < MAX_HISTORY) {
+        history2[history_count].a = a;
+        history2[history_count].op1 = op1;
+        history2[history_count].b = b;
+        history2[history_count].op2 = op2;
+        main_history[history_count].problem == "%i %c = %i %c\n", a, op1, b, op2;
+        history_count++;
+    } else {
+        printf("History is full!\n");
+    }
+}
+
+void view_history() {
+    if (history_count == 0) {
+        printf("History is empty.\n");
+    }
+    for (int i = 0; i < history_count; i++) {
+        printf("%s\n", main_history[i].problem);
+    }
 }
