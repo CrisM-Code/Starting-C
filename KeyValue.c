2836720108ae223a771c379a2c6756e46ee4c53e
Cris M <crismecea1@outlook.com>
2026-01-19 20:19:41 -0500

    Added Comments and fixed the file layout/persistence

--- KeyValue.c
+++ KeyValue.c
@@ -40,32 +40,49 @@ int main(void) {
     load_values();
     
     while (1) {
+
+        //Gets the user input
         printf(">");
         fgets(text, sizeof(text), stdin);
         text[strcspn(text, "\n")] = '\0';
 
+        //If user enters 'quit' then the program will stop
         if (strcmp(text, "quit")==0) {
             printf("Exiting Program...\n");
             break;
         }
 
+        //This scans the input and assigns values
         sscanf(text, "%3s %9s", command, key2);
 
+        //If the user enters the get function then they can retrieve the value
         if (strcmp(command, "get") == 0) {
             int index = hash_function(key2);
-            printf("%s", store[index].Value);
+            printf("%s\n", store[index].Value);
         } 
+
+        //If the user enters the set function then they can set a value to a key
         else if (strcmp(command, "set") == 0) {
+
+            //This checks if the storage is full
             if (value_count >= 23) {
                 printf("Storage is full!\n");
                 break;
             }
             
+            //First, this turns the key into a hash number. That number will be the array index
             int index = hash_function(key2);
+
+            //Second, it asks the user to enter a value. That value will get saved to the store struct
             printf("Enter a Value: ");
             fgets(store[index].Value, sizeof(store[index].Value), stdin);
+            store[index].Value[strcspn(store[index].Value, "\n")] = '\0';
+
+            //Third, it prints value has been saved and increments the value count
             printf("Value has been saved!\n");
             value_count++;
+            
+            //This line will copy the key to the store struct
             strcpy(store[index].key, key2);
             fp = fopen("KeyValue.txt", "w");
             for (int i = 0; i < 23; i++) {
@@ -73,6 +90,9 @@ int main(void) {
             }
             fclose(fp);
         } 
+
+        //The del command will assign the users value with empty. I haven't figured out a better way yet
+        //Possibly, I can use pointers and reference the unused values as NULL
         else if (strcmp(command, "del")==0) {
             int index = hash_function(key2);
             strcpy(store[index].Value, "Empty\n");
@@ -82,11 +102,15 @@ int main(void) {
             }
             fclose(fp);
         } 
+
+        //This prints a help menu explaining the program. Similar to a Linux or Git terminal
         else if (strcmp(text, "help")==0) {
             printf("==== Help Menu ====\n\nset: enter set <key> and you will be able to enter a value to store\n");
             printf("get: enter get <key> and the value associated with the key will be returned.\n");
             printf("del: enter del <key> and the value associated with the key will be deleted.\n");
         } else {
+
+            //In case the user enters something that is not part of the program
             printf("Error!\n");
         }
     }
@@ -97,6 +121,7 @@ int main(void) {
     return 0;
 }
 
+//Scans the file and recreates it for persistence
 void load_values() {
     fp = fopen("KeyValue.txt", "r");
     char line[40];
